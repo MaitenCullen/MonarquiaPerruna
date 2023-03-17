0561caa36280664aee5c9cfbeafd4ec213218dc5
Maiten Cullen <maitencullen@gmail.com>
2023-03-17 16:30:03 -0300

    confi de taildwind css y daisyui andando, colores base en config

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-     <h1 class="text-3xl underline">
+     <h1 class="text-3xl underline text-yellow-400">
     Hello world!
   </h1>
   <button class="btn bg-redPrimary">One</button>
