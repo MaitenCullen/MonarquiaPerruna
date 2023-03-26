12c4bae0dd4864e406a86574095416cf65ba6184
Maiten Cullen <maitencullen@gmail.com>
2023-03-26 13:08:25 -0300

    card contact component

--- src/components/CardContact.jsx
+++ src/components/CardContact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import img3 from '/img/img3.png'
 
+
 export const CardContact = () => {
   return (
-    <div>CardContact
-        <img src={img3}/>
+    <div>
+      <div>
+         <img src={img3}/>
+      </div>
+      <div>
+        <p>
+          Si tienes alguna duda o consulta, comunícate con nosotros. Estaremos gustosos de atenderte.
+        </p>
+        <button className="btn bg-redPrimary btn-xs sm:btn-sm md:btn-md lg:btn-lg buttonClass hover:bg-yellowPrimary border-current hover:border-current">
+          Contáctanos
+        </button>
+      </div>
     </div>
   )
 }
