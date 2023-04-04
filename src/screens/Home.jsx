ed09c3aa95a782bb77682a2a74bd158bc0fc9197
delfinblaksleymujica <delf.blaksley@hotmail.com>
2023-04-03 22:31:20 -0300

    Navbar, carousel principal , y titulo de fortalezas ( los links no estan hechos )

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,11 +3,15 @@ import { CardContact } from '../components/CardContact';
 import '../styles.css'
 import { Testimonios } from './../section/Testimonios';
 import { Contact } from './../section/Contact';
+import MainCarousel from '../components/MainCarousel';
+import Fortalezas from '../section/Fortalezas';
 
 
 export const Home = () => {
   return (
     <div>
+      <MainCarousel/>
+      <Fortalezas/>
       <Testimonios/>
       <Contact/>
     </div>
