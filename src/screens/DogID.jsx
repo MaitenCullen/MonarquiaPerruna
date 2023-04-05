394961506acb06807d294f808689d63368043bc0
Maiten Cullen <maitencullen@gmail.com>
2023-04-05 15:42:35 -0300

    nueva seccion de app dog id

--- src/screens/DogID.jsx
+++ src/screens/DogID.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { CardDogs } from '../components/CardDogs'
 import { CaracteriticasDogId } from '../components/CaracteriticasDogId'
+import { ComoSeUsa } from './ComoSeUsa'
 
 export const DogID = () => {
   return (
-    <div className='w-screen'>
-      <CaracteriticasDogId/>
-      <CardDogs/>
+    <div className='w-screen flex justify-center'>
+      <ComoSeUsa/>
     </div>
   )
 }
