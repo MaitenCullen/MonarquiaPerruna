de06702cb2127a6b049a2dab261dd07cfd6bc4e7
Maiten Cullen <maitencullen@gmail.com>
2023-04-06 18:31:05 -0300

    pagina app info y app instalada en proceso

---
+++ src/components/AppDogID.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import salud from '/img/cruz.png';
+import ok from '/img/ok.png';
+import circulo from '/img/circulo.png'
+
+export const AppDogID = () => {
+  return (
+    <div className='containerDOGID'>
+         <div className='divAPP'>
+            <div className='APPdogID'>
+            <img src={salud} className='imgAPP'/>
+              <div className='divAPPInfo '>
+                <div className="divTextAPP">
+                    <h4>HISTORIA MÉDICA</h4>
+                    <p>
+                    Con solo acercar tu celular al DOG ID podrás ver la historia médica de tu mascota como enfermedades o cuidados especiales.
+                    </p>
+                </div>
+            </div>
+            </div>
+          </div>
+          <div className='divAPP'>
+            <div className='APPdogID'>
+            <img src={ok} className='imgAPP'/>
+              <div className='divAPPInfo '>
+                <div className="divTextAPP">
+                    <h4>FÁCIL DE USAR</h4>
+                    <p>
+                    Registrar la información en la app Dog Id toma unos minutos. Luego con solo acercar el celular al Dog Id podrás ver toda la información registrada.                    </p>
+                </div>
+            </div>
+            </div>
+          </div>
+          <div className='divAPP'>
+            <div className='APPdogID'>
+            <img src={salud} className='imgAPP'/>
+              <div className='divAPPInfo '>
+                <div className="divTextAPP">
+                    <h4>FÁCIL ACCESO</h4>
+                    <p>
+                    Con un celular Android o iOS podrás acceder a toda la información de tu mascota y tus datos de contacto.                    </p>
+                </div>
+            </div>
+            </div>
+          </div>
+          <div className='divAPP'>
+            <div className='APPdogID'>
+            <img src={circulo} className='imgAPP'/>
+              <div className='divAPPInfo '>
+                <div className="divTextAPP">
+                    <h4>DATOS ACTUALIZABLES</h4>
+                    <p>
+                    Puedes actualizar o borrar los datos registrados en la app las veces que desees.                     </p>
+                </div>
+            </div>
+            </div>
+          </div>
+    </div>
+  )
+}
