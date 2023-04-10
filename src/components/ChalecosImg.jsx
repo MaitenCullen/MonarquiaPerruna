a21c4943aea37164967bf3055d0ff2401911e24c
Maiten Cullen <maitencullen@gmail.com>
2023-04-10 15:41:08 -0300

    seccion chalecos

---
+++ src/components/ChalecosImg.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/CardStyles.css';
+
+export const ChalecosImg = () => {
+  return (
+    <div className='flex flex-wrap justify-evenly w-11/12'>
+        <div className='p-1 m-1 imgProducto' id='chaleco01'></div>
+        <div className='p-1 m-1 imgProducto' id='chaleco02'></div>
+        <div className='p-1 m-1 imgProducto' id='chaleco03'></div>
+        <div className='p-1 m-1 imgProducto' id='chaleco04'></div>
+        <div className='p-1 m-1 imgProducto' id='chaleco05'></div>
+        <div className='p-1 m-1 imgProducto' id='chaleco06'></div>
+  </div>
+  )
+}
