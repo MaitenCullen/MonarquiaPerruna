7714838eb8962ff22f1ef8d7984bc14210b2a8b3
Maximiliano Castro <m.castro@ptytechnologies.com>
2023-05-12 21:09:04 +0200

    modifique el hover

--- src/components/ChalecosImg.jsx
+++ src/components/ChalecosImg.jsx
@@ -4,7 +4,7 @@ import '../styles/CardStyles.css';
 export const ChalecosImg = () => {
   return (
     <div className='flex flex-wrap justify-evenly w-11/12'>
-        <div className='p-1 m-1 imgProducto' id='chaleco01'></div>
+        <div className='p-1 m-1 imgProducto mediaRopa' id='chaleco01'></div>
         <div className='p-1 m-1 imgProducto' id='chaleco02'></div>
         <div className='p-1 m-1 imgProducto' id='chaleco03'></div>
         <div className='p-1 m-1 imgProducto' id='chaleco04'></div>
